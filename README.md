d8debe97b8136776220649c9cf4f9521e5f7daf8
yuriy_beloff <paganismrus@gmail.com>
2018-04-10 19:33:16 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+This module checks urls in file with domains and shows health status (status code and expiration date)
+
+# How to use
+
+```
+[user@server]$ python3.5 check_sites_health.py --path urls.txt
+
+Domain: http://www.ya.ru has status code 200 and expire in 112 days, 1:46:53.908128
+
+--path is required parameter and it points to file with urls
+
+```
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,62 @@
-def load_urls4check(path):
-    pass
+import whois
+import argparse
+import os
+import requests
+import sys
+from datetime import datetime
+from datetime import timedelta
+
+
+def load_urls_from_file(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            url_list = file.read().splitlines()
+        return url_list
+    except ValueError:
+        return False
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        status_code = requests.get(url).status_code
+        return status_code
+    except ValueError:
+        return None
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    try:
+        domain_info = whois.query(domain_name)
+        return domain_info.expiration_date
+    except ValueError:
+        return None
+
+
+def check_expiration_date(expiration_date):
+    delta = expiration_date - datetime.now()
+    return delta
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path',
+        dest='path',
+        required=True,
+        help='Path to file'
+    )
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    args = parse_arguments()
+    if not os.path.exists(args.path):
+        sys.exit('Path does not exist')
+    filepath = args.path
+    url_list = load_urls_from_file(filepath)
+    for url in url_list:
+        status_code = (is_server_respond_with_200(url))
+        if status_code:
+            exp_date = (get_domain_expiration_date(url))
+            delta = check_expiration_date(exp_date)
+        print('Domain: {} has status code {} and expire in {}'.format(url, status_code, delta))
