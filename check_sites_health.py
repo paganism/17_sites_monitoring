08e673d82a2f5a36e2e5f33abd3ebba155220a5a
yuriy_beloff <paganismrus@gmail.com>
2018-04-11 23:10:38 +0300

    modified user output

--- check_sites_health.py
+++ check_sites_health.py
@@ -3,8 +3,8 @@ import argparse
 import os
 import requests
 import sys
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
+from urllib.parse import urlparse
 
 
 def load_urls_from_file(filepath):
@@ -13,28 +13,36 @@ def load_urls_from_file(filepath):
             url_list = file.read().splitlines()
         return url_list
     except ValueError:
-        return False
+        return None
+
+
+def get_domain_name(url):
+    domain_name = urlparse(url).netloc
+    return domain_name
 
 
 def is_server_respond_with_200(url):
     try:
-        status_code = requests.get(url).status_code
-        return status_code
+        response = requests.get(url)
+        return response.ok
     except ValueError:
-        return None
+        return False
 
 
 def get_domain_expiration_date(domain_name):
     try:
         domain_info = whois.query(domain_name)
         return domain_info.expiration_date
-    except ValueError:
+    except AttributeError:
         return None
 
 
 def check_expiration_date(expiration_date):
-    delta = expiration_date - datetime.now()
-    return delta
+    try:
+        delta = expiration_date - datetime.now()
+        return delta
+    except ValueError:
+        return None
 
 
 def parse_arguments():
@@ -55,8 +63,17 @@ if __name__ == '__main__':
     filepath = args.path
     url_list = load_urls_from_file(filepath)
     for url in url_list:
-        status_code = (is_server_respond_with_200(url))
-        if status_code:
-            exp_date = (get_domain_expiration_date(url))
-            delta = check_expiration_date(exp_date)
-        print('Domain: {} has status code {} and expire in {}'.format(url, status_code, delta))
+        domain_name = get_domain_name(url)
+        status_code = is_server_respond_with_200(url)
+        exp_date = get_domain_expiration_date(domain_name)
+        if not status_code or not exp_date:
+            print('Domain: | {} | is not healthy'.format(domain_name))
+            print('-----------------------------------------')
+        else:
+            delta_time = check_expiration_date(exp_date)
+            if delta_time >= timedelta(30):
+                print('Domain: | {} | healthy and expire in {}'.format(
+                    domain_name,
+                    delta_time)
+                )
+                print('-----------------------------------------')
